bb3010575d84ff2820f7f8ef755e04189b1d16a7
[mbax4sd2] <[simon.davis-3@student.manchester.ac.uk]>
2017-09-27 15:11:32 +0100

    Prints average RGB value of opened image

--- mosaic.py
+++ mosaic.py
@@ -2,3 +2,14 @@ from PIL import Image
 
 TargetImage = Image.open('monkey.jpg')
 TargetImage.show()
+
+width, height = TargetImage.size
+print width, "X", height
+
+PixelValues = list(TargetImage.getdata())
+RValues = [x[0] for x in PixelValues]
+GValues = [x[1] for x in PixelValues]
+BValues = [x[2] for x in PixelValues]
+
+AverageRGBValue = ( sum(RValues)/len(RValues), sum(GValues)/len(GValues), sum(BValues)/len(BValues) )
+print AverageRGBValue
