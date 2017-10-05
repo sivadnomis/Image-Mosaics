b134daa9fb269e623a3ccf5ba70893ea50746105
[mbax4sd2] <[simon.davis-3@student.manchester.ac.uk]>
2017-10-05 17:32:19 +0100

    put code blocks into functions

--- mosaic.py
+++ mosaic.py
@@ -4,67 +4,68 @@ import os
 ##################
 #open source image
 ##################
-TargetImage = Image.open('monkey.jpg')
-
-source_size = 500, 500
-#TargetImage = ImageOps.fit(TargetImage, thumbnail_size, Image.ANTIALIAS)
-TargetImage.thumbnail(source_size, Image.ANTIALIAS)
-#TargetImage.show()
+def ShowSourceImage():
+  TargetImage = Image.open('monkey.jpg')
+  source_size = 500, 500
+  #TargetImage = ImageOps.fit(TargetImage, thumbnail_size, Image.ANTIALIAS)
+  TargetImage.thumbnail(source_size, Image.ANTIALIAS)
+  TargetImage.show()
 
 ##################
 #open tile images in library
 ##################
-tile_size = 50, 50
-os.chdir(r'library')
-for f in os.listdir(os.getcwd()):#os.listdir('library'):
-    #print f
-    tile = Image.open(f)
-    tile.thumbnail(tile_size, Image.ANTIALIAS)
-    #tile.show()
+def ShowLibraryImages():
+  tile_size = 50, 50
+  os.chdir(r'library')
+  for f in os.listdir(os.getcwd()):#os.listdir('library'):
+      #print f
+      tile = Image.open(f)
+      tile.thumbnail(tile_size, Image.ANTIALIAS)
+      tile.show()
 
 ##################
 #join 4 images together
 ##################
-images = map(Image.open, ['bronze.jpg', 'hut.jpg'])
-images.append(Image.open("frog.jpg"))
-images.append(Image.open("raven.jpg"))
+def FourImageMosaic():
+  images = map(Image.open, ['bronze.jpg', 'hut.jpg'])
+  images.append(Image.open("frog.jpg"))
+  images.append(Image.open("raven.jpg"))
 
-for i in images:
-  i.thumbnail(tile_size, Image.ANTIALIAS)
-widths, heights = zip(*(i.size for i in images))
+  for i in images:
+    i.thumbnail(tile_size, Image.ANTIALIAS)
+  widths, heights = zip(*(i.size for i in images))
 
-total_width = sum(widths)/2
-total_height = sum(heights)/2
-#max_height = max(heights)
+  total_width = sum(widths)/2
+  total_height = sum(heights)/2
+  #max_height = max(heights)
 
-mosaic = Image.new('RGB', (total_width, total_height))
+  mosaic = Image.new('RGB', (total_width, total_height))
 
-x_offset = 0
-y_offset = 0
-tile_index = 0
-for im in images:
-  mosaic.paste(im, (x_offset,y_offset))
-  #print "pasted image"
-  #print len(images)/2
-  if tile_index > (len(images)/2) - 2:
-    tile_index = 0
-    x_offset = 0
-    y_offset += heights[0]
-    #print "x,y offset is: ", x_offset, y_offset
-  else:
-    x_offset += widths[0]
-    tile_index += 1
-    #print "x,y offset is: ", x_offset, y_offset
-  
-  #print tile_index
+  x_offset = 0
+  y_offset = 0
+  tile_index = 0
+  for im in images:
+    mosaic.paste(im, (x_offset,y_offset))
+    #print "pasted image"
+    #print len(images)/2
+    if tile_index > (len(images)/2) - 2:
+      tile_index = 0
+      x_offset = 0
+      y_offset += heights[0]
+      #print "x,y offset is: ", x_offset, y_offset
+    else:
+      x_offset += widths[0]
+      tile_index += 1
+      #print "x,y offset is: ", x_offset, y_offset
+    
+    #print tile_index
 
-#mosaic.save('test.jpg')
-#mosaic.show()
+  #mosaic.save('test.jpg')
+  mosaic.show()
 
 ##################
 #calculate average RGB of source image
 ##################
-
 def CalcAverageRGB( image ):
   width, height = image.size
   #print width, "X", height
@@ -81,21 +82,24 @@ def CalcAverageRGB( image ):
 ##################
 #divide source image into blocks for replacement
 ##################
-
 def DivideImageIntoBlocks( image, block_x, block_y, output_dict ):
   width, height = image.size
 
   for i in range(0, width, block_x):
     for j in range(0, height, block_y):
-      #print i, j
       cropped_image = image.crop((i, j, i+block_x, j+block_y))
       output_dict[cropped_image] = CalcAverageRGB( cropped_image )
       #cropped_image.show()
-      #CalcAverageRGB(cropped_image)
 
+##################
+#main method
+##################
 
+#ShowSourceImage()
+#ShowLibraryImages()
+#FourImageMosaic()
 CalcAverageRGB(TargetImage)
 
 block_dict = {}
 DivideImageIntoBlocks(TargetImage, 50, 50, block_dict)
-print block_dict
+#print block_dict
