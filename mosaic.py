ea6f12f9761feb62bcbe73d932da38d74ce665d3
[mbax4sd2] <[simon.davis-3@student.manchester.ac.uk]>
2017-10-05 16:16:12 +0100

    shows cropped section of source image + added 2 more tile pictures

--- mosaic.py
+++ mosaic.py
@@ -6,30 +6,31 @@ import os
 ##################
 TargetImage = Image.open('monkey.jpg')
 
-thumbnail_size = 250, 250
-TargetImage = ImageOps.fit(TargetImage, thumbnail_size, Image.ANTIALIAS)
-#TargetImage.thumbnail(thumbnail_size, Image.ANTIALIAS)
-#TargetImage.show()
+source_size = 500, 500
+#TargetImage = ImageOps.fit(TargetImage, thumbnail_size, Image.ANTIALIAS)
+TargetImage.thumbnail(source_size, Image.ANTIALIAS)
+TargetImage.show()
 
 ##################
 #open tile images in library
 ##################
+tile_size = 50, 50
 os.chdir(r'library')
 for f in os.listdir(os.getcwd()):#os.listdir('library'):
     print f
     tile = Image.open(f)
-    tile.thumbnail(thumbnail_size, Image.ANTIALIAS)
+    tile.thumbnail(tile_size, Image.ANTIALIAS)
     #tile.show()
 
 ##################
 #join 4 images together
 ##################
 images = map(Image.open, ['bronze.jpg', 'hut.jpg'])
-images.append(Image.open("bronze.jpg"))
-images.append(Image.open("hut.jpg"))
+images.append(Image.open("frog.jpg"))
+images.append(Image.open("raven.jpg"))
 
 for i in images:
-  i.thumbnail(thumbnail_size, Image.ANTIALIAS)
+  i.thumbnail(tile_size, Image.ANTIALIAS)
 widths, heights = zip(*(i.size for i in images))
 
 total_width = sum(widths)/2
@@ -43,22 +44,22 @@ y_offset = 0
 tile_index = 0
 for im in images:
   mosaic.paste(im, (x_offset,y_offset))
-  print "pasted image"
-
-  if tile_index > 0:
+  #print "pasted image"
+  #print len(images)/2
+  if tile_index > (len(images)/2) - 2:
     tile_index = 0
     x_offset = 0
     y_offset += heights[0]
-    print "x,y offset is: ", x_offset, y_offset
+    #print "x,y offset is: ", x_offset, y_offset
   else:
     x_offset += widths[0]
     tile_index += 1
-    print "x,y offset is: ", x_offset, y_offset
+    #print "x,y offset is: ", x_offset, y_offset
   
-  print tile_index
+  #print tile_index
 
-mosaic.save('test.jpg')
-mosaic.show()
+#mosaic.save('test.jpg')
+#mosaic.show()
 
 ##################
 #calculate average RGB of source image
@@ -73,3 +74,6 @@ BValues = [x[2] for x in PixelValues]
 
 AverageRGBValue = ( sum(RValues)/len(RValues), sum(GValues)/len(GValues), sum(BValues)/len(BValues) )
 print AverageRGBValue
+
+cropped_image = TargetImage.crop((0, 0, 50, 50))
+cropped_image.show()
