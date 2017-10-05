36b1c2d4f75ef5473fb6c50859daa85fd62ea6c5
[mbax4sd2] <[simon.davis-3@student.manchester.ac.uk]>
2017-10-05 14:08:42 +0100

    produces a 2x2 grid of 4 tiles

--- mosaic.py
+++ mosaic.py
@@ -1,12 +1,14 @@
-from PIL import Image
+from PIL import Image, ImageOps
 import os
 
 ##################
 #open source image
 ##################
 TargetImage = Image.open('monkey.jpg')
-size = 250, 250
-TargetImage.thumbnail(size, Image.ANTIALIAS)
+
+thumbnail_size = 250, 250
+TargetImage = ImageOps.fit(TargetImage, thumbnail_size, Image.ANTIALIAS)
+#TargetImage.thumbnail(thumbnail_size, Image.ANTIALIAS)
 #TargetImage.show()
 
 ##################
@@ -16,29 +18,44 @@ os.chdir(r'library')
 for f in os.listdir(os.getcwd()):#os.listdir('library'):
     print f
     tile = Image.open(f)
-
-    size = 250, 250
-    tile.thumbnail(size, Image.ANTIALIAS)
+    tile.thumbnail(thumbnail_size, Image.ANTIALIAS)
     #tile.show()
 
 ##################
-#join 2 images together
+#join 4 images together
 ##################
 images = map(Image.open, ['bronze.jpg', 'hut.jpg'])
-size = 250, 250
+images.append(Image.open("bronze.jpg"))
+images.append(Image.open("hut.jpg"))
+
 for i in images:
-  i.thumbnail(size, Image.ANTIALIAS)
+  i.thumbnail(thumbnail_size, Image.ANTIALIAS)
 widths, heights = zip(*(i.size for i in images))
 
-total_width = sum(widths)
-max_height = max(heights)
+total_width = sum(widths)/2
+total_height = sum(heights)/2
+#max_height = max(heights)
 
-mosaic = Image.new('RGB', (total_width, max_height))
+mosaic = Image.new('RGB', (total_width, total_height))
 
 x_offset = 0
+y_offset = 0
+tile_index = 0
 for im in images:
-  mosaic.paste(im, (x_offset,0))
-  x_offset += im.size[0]
+  mosaic.paste(im, (x_offset,y_offset))
+  print "pasted image"
+
+  if tile_index > 0:
+    tile_index = 0
+    x_offset = 0
+    y_offset += heights[0]
+    print "x,y offset is: ", x_offset, y_offset
+  else:
+    x_offset += widths[0]
+    tile_index += 1
+    print "x,y offset is: ", x_offset, y_offset
+  
+  print tile_index
 
 mosaic.save('test.jpg')
 mosaic.show()
