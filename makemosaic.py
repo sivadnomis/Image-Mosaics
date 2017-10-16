e9653aa7653fbc439f7ee16a8e775609f3de9527
[mbax4sd2] <[simon.davis-3@student.manchester.ac.uk]>
2017-10-16 13:35:54 +0100

    modified function.variable names to fit PEP8 standards

--- makemosaic.py
+++ makemosaic.py
@@ -2,4 +2,4 @@ import mosaic
 import sys
 
 print ("Creating mosaic for: ", sys.argv[1], "with tile size: ", int(sys.argv[2]))
-mosaic.createMosaic(sys.argv[1], int(sys.argv[2]))
+mosaic.create_mosaic(sys.argv[1], int(sys.argv[2]))
--- mosaic.py
+++ mosaic.py
@@ -5,10 +5,10 @@ import math
 ##################
 #open source image
 ##################
-def ResizeSourceImage( image ):
+def resize_source_image( image ):
   
   source_size = 500, 500
-  #TargetImage = ImageOps.fit(TargetImage, thumbnail_size, Image.ANTIALIAS)
+  #target_image = ImageOps.fit(target_image, thumbnail_size, Image.ANTIALIAS)
   image.thumbnail(source_size, Image.ANTIALIAS)
   print image.size
   image.show()
@@ -17,7 +17,7 @@ def ResizeSourceImage( image ):
 ##################
 #open tile images in library
 ##################
-def ResizeLibraryImages(tile_size): 
+def resize_library_images(tile_size): 
   images = []
   os.chdir(r'library')
   for f in os.listdir(os.getcwd()):#os.listdir('library'):
@@ -27,43 +27,9 @@ def ResizeLibraryImages(tile_size):
   return images
 
 ##################
-#join 4 images together
-##################
-def FourImageMosaic(tile_size):
-  images = map(Image.open, ['bronze.jpg', 'hut.jpg'])
-  images.append(Image.open("frog.jpg"))
-  images.append(Image.open("raven.jpg"))
-
-  for i in images:
-    i.thumbnail(tile_size, Image.ANTIALIAS)
-  widths, heights = zip(*(i.size for i in images))
-
-  total_width = sum(widths)/2
-  total_height = sum(heights)/2
-  #max_height = max(heights)
-
-  mosaic = Image.new('RGB', (total_width, total_height))
-
-  x_offset = 0
-  y_offset = 0
-  tile_index = 0
-  for im in images:
-    mosaic.paste(im, (x_offset,y_offset))
-    if tile_index > (len(images)/2) - 2:
-      tile_index = 0
-      x_offset = 0
-      y_offset += heights[0]
-    else:
-      x_offset += widths[0]
-      tile_index += 1
-
-  #mosaic.save('test.jpg')
-  mosaic.show()
-
-##################
 #calculate average RGB of source image
 ##################
-def CalcAverageRGB( image ):
+def calc_average_rgb( image ):
   width, height = image.size
 
   PixelValues = list(image.getdata())
@@ -71,13 +37,13 @@ def CalcAverageRGB( image ):
   GValues = [x[1] for x in PixelValues]
   BValues = [x[2] for x in PixelValues]
 
-  AverageRGBValue = ( sum(RValues)/len(RValues), sum(GValues)/len(GValues), sum(BValues)/len(BValues) )
-  return AverageRGBValue
+  average_rgb_value = ( sum(RValues)/len(RValues), sum(GValues)/len(GValues), sum(BValues)/len(BValues) )
+  return average_rgb_value
 
 ##################
 #divide source image into blocks for replacement
 ##################
-def DivideImageIntoBlocks( image, tile_size, output_dict ):
+def divide_image_into_blocks( image, tile_size, output_dict ):
   width, height = image.size
   #print "width: ", width
   #print "height", height
@@ -86,7 +52,7 @@ def DivideImageIntoBlocks( image, tile_size, output_dict ):
   for i in range(0, width, tile_size[0]):
     for j in range(0, height, tile_size[1]):
       cropped_image = image.crop((i, j, i+tile_size[0], j+tile_size[1]))
-      output_dict[coordinate] = CalcAverageRGB( cropped_image )
+      output_dict[coordinate] = calc_average_rgb( cropped_image )
       coordinate += 1
 
   return ((i,j))
@@ -97,51 +63,51 @@ def distance( x , y ):
 ##################
 #main method - construct mosaic from tiles in library
 ##################
-def createMosaic(source_image, input_tile_size):
-  TargetImage = Image.open(source_image)
+def create_mosaic(source_image, input_tile_size):
+  target_image = Image.open(source_image)
   tile_size = input_tile_size, input_tile_size
   print "tile size is : ", tile_size
 
-  TargetImage = ResizeSourceImage(TargetImage)
+  target_image = resize_source_image(target_image)
   tiles = {}
-  tiles = ResizeLibraryImages(tile_size)
+  tiles = resize_library_images(tile_size)
   #FourImageMosaic(tile_size)
 
   #Key = RGB, Value = Image
-  TileRGBAverages = {}
+  tile_rgb_averages = {}
   for t in tiles:
-    TileRGBAverages[CalcAverageRGB(t)] = t
+    tile_rgb_averages[calc_average_rgb(t)] = t
 
   #Key = coordinate, Value = rgb
-  blockRGB_dict = {}
-  CroppedImageXY = DivideImageIntoBlocks(TargetImage, tile_size, blockRGB_dict)
+  block_rgb_dict = {}
+  cropped_image_xy = divide_image_into_blocks(target_image, tile_size, block_rgb_dict)
 
-  #print "tilergbaverages keys: ", TileRGBAverages.keys()
-  #print "tilergbaverages values: ", TileRGBAverages.values()
-  #print "blockRGB_dict keys: ", blockRGB_dict.keys()
-  #print "blockRGB_dict values: ", blockRGB_dict.values()
+  #print "tilergbaverages keys: ", tile_rgb_averages.keys()
+  #print "tilergbaverages values: ", tile_rgb_averages.values()
+  #print "block_rgb_dict keys: ", block_rgb_dict.keys()
+  #print "block_rgb_dict values: ", block_rgb_dict.values()
 
   #finds the closest tile colour to the first block of the source image
-  first_blocks_tile = min(TileRGBAverages.keys(), key=lambda x:distance(x, blockRGB_dict.values()[0]))
+  first_blocks_tile = min(tile_rgb_averages.keys(), key=lambda x:distance(x, block_rgb_dict.values()[0]))
   #print (first_blocks_tile)
 
   #gives us the tile that best matches the first block in the source image
 
-  widths, heights = zip(*(i.size for i in TileRGBAverages.values()))
+  widths, heights = zip(*(i.size for i in tile_rgb_averages.values()))
 
-  #print TargetImage.size
-  mosaic = Image.new('RGB', (TargetImage.size[0], TargetImage.size[1]))
+  #print target_image.size
+  mosaic = Image.new('RGB', (target_image.size[0], target_image.size[1]))
 
   x_offset = 0
   y_offset = 0
 
-  for i in range(0, len(blockRGB_dict.keys()), 1):
+  for i in range(0, len(block_rgb_dict.keys()), 1):
     #find closest colour tile in library to this specific block
-    tile_to_replace_block = min(TileRGBAverages.keys(), key=lambda x:distance(x, blockRGB_dict.values()[i]))
-    mosaic.paste(TileRGBAverages.get(tile_to_replace_block), (x_offset,y_offset))
+    tile_to_replace_block = min(tile_rgb_averages.keys(), key=lambda x:distance(x, block_rgb_dict.values()[i]))
+    mosaic.paste(tile_rgb_averages.get(tile_to_replace_block), (x_offset,y_offset))
 
     #sets the point to place the next tile
-    if y_offset < CroppedImageXY[1]:
+    if y_offset < cropped_image_xy[1]:
       y_offset += tile_size[1]#heights[0]
     else:
       y_offset = 0
