63df267203bce85fe3eb7441b86a969a0c7abee6
[mbax4sd2] <[simon.davis-3@student.manchester.ac.uk]>
2017-10-20 13:33:14 +0100

    cleaned code for v1 release

--- makemosaic.py
+++ makemosaic.py
@@ -1,5 +1,5 @@
 import mosaic
 import sys
 
-print ("Creating mosaic for: ", sys.argv[1], "with tile size: ", int(sys.argv[2]))
+print "Creating mosaic for:", sys.argv[1], "with tile size:", int(sys.argv[2])
 mosaic.create_mosaic(sys.argv[1], int(sys.argv[2]))
--- mosaic.py
+++ mosaic.py
@@ -8,9 +8,7 @@ import math
 def resize_source_image( image ):  
   #default dimensions to resize source image to
   source_size = 500, 500
-  #target_image = ImageOps.fit(target_image, thumbnail_size, Image.ANTIALIAS)
   image.thumbnail(source_size, Image.ANTIALIAS)
-  print image.size
   image.show()
   return image
 
@@ -20,7 +18,7 @@ def resize_source_image( image ):
 def resize_library_images(tile_size): 
   images = []
   os.chdir(r'library')
-  for f in os.listdir(os.getcwd()):#os.listdir('library'):
+  for f in os.listdir(os.getcwd()):
       tile = Image.open(f)
       tile.thumbnail(tile_size, Image.ANTIALIAS)
       images.append(tile)
@@ -45,8 +43,6 @@ def calc_average_rgb( image ):
 ##################
 def divide_image_into_blocks( image, tile_size, output_dict ):
   width, height = image.size
-  #print "width: ", width
-  #print "height", height
 
   coordinate = 0;
   for i in range(0, width, tile_size[0]):
@@ -57,6 +53,9 @@ def divide_image_into_blocks( image, tile_size, output_dict ):
 
   return ((i,j))
 
+##################
+#get euclidean distance between 2 RGB values
+##################
 def distance( x , y ):
   return math.sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2)
 
@@ -66,12 +65,10 @@ def distance( x , y ):
 def create_mosaic(source_image, input_tile_size):
   target_image = Image.open(source_image)
   tile_size = input_tile_size, input_tile_size
-  print "tile size is : ", tile_size
 
   target_image = resize_source_image(target_image)
   tiles = {}
   tiles = resize_library_images(tile_size)
-  #FourImageMosaic(tile_size)
 
   #Key = RGB, Value = Image
   tile_rgb_averages = {}
@@ -82,15 +79,9 @@ def create_mosaic(source_image, input_tile_size):
   block_rgb_dict = {}
   cropped_image_xy = divide_image_into_blocks(target_image, tile_size, block_rgb_dict)
 
-  #print "tilergbaverages keys: ", tile_rgb_averages.keys()
-  #print "tilergbaverages values: ", tile_rgb_averages.values()
-  #print "block_rgb_dict keys: ", block_rgb_dict.keys()
-  #print "block_rgb_dict values: ", block_rgb_dict.values()
-
   #gives us the tile that best matches the first block in the source image
   widths, heights = zip(*(i.size for i in tile_rgb_averages.values()))
-
-  #print target_image.size
+  #create base mosaic image of default dimensions
   mosaic = Image.new('RGB', (target_image.size[0], target_image.size[1]))
 
   x_offset = 0
@@ -101,26 +92,21 @@ def create_mosaic(source_image, input_tile_size):
   for i in range(0, len(block_rgb_dict.keys()), 1):    
     #find closest colour tile in library to this specific block
     tile_to_replace_block = min(tile_rgb_averages.keys(), key=lambda x:distance(x, block_rgb_dict.values()[i]))
-    #print "source block ", block_rgb_dict.values()[i], " matches ", tile_to_replace_block
+    #paste tile into place in output mosaic image
     mosaic.paste(tile_rgb_averages.get(tile_to_replace_block), (x_offset,y_offset))
 
     #sets the point to place the next tile
     if y_offset < cropped_image_xy[1]:
-      y_offset += tile_size[1]#heights[0]
+      y_offset += tile_size[1]
     else:
       y_offset = 0
-      x_offset += tile_size[0]#widths[0]
+      x_offset += tile_size[0]
 
     num_tiles_placed += 1
     progress_percentage = num_tiles_placed/float(len(block_rgb_dict.keys())) * 100
-    if progress_percentage % 1 == 0:
+    if progress_percentage % 5 == 0:
       print "Progress: ", progress_percentage, "%"
 
   os.path.splitext(source_image)[0]
   mosaic.save('/home/mbax4sd2/3rd Year Project/output/%s%smosaic.jpg' % (os.path.splitext(source_image)[0], input_tile_size)) 
   mosaic.show()
-
-#TODO: percentage progress bar for output
-
-#command for generating a diff on 2 images. good enough for difference testing?
-#compare -metric PSNR mesmall.jpg output/me2mosaic.jpg output/diff.jpg
