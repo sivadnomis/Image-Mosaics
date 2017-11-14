5be72d23a79d79fb161fde5423eb681e715375f9
[mbax4sd2] <[simon.davis-3@student.manchester.ac.uk]>
2017-11-14 16:38:02 +0000

    add tile image in library to our database

--- processlibrary.py
+++ processlibrary.py
@@ -1,18 +1,31 @@
-import mosaic
-import sqlite3
+from PIL import Image, ImageOps
+import mosaic, os
+import sqlite3, imagehash
 
 sqlite_file = 'image_library'
 
 db = sqlite3.connect(sqlite_file)
 cursor = db.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS tiles(file_name TEXT PRIMARY KEY, RValue INT, GValue INT, BValue, INT, hash INT)")
+cursor.execute("CREATE TABLE IF NOT EXISTS tiles(file_name TEXT PRIMARY KEY, RValue INT, GValue INT, BValue INT)")
 
 cursor.execute("SELECT * FROM tiles")
 print cursor.fetchall()
 
+counter = 0
+os.chdir(r'library')
+for f in os.listdir(os.getcwd()):
+    tile = Image.open(f)
+
+    tile_RGB = mosaic.calc_average_rgb(tile, False, False)
+    tile_RValue = tile_RGB[0]
+    tile_GValue = tile_RGB[1]
+    tile_BValue = tile_RGB[2]
+
+    cursor.execute('''INSERT OR IGNORE INTO tiles(file_name, RValue, GValue, BValue)
+                  VALUES(?,?,?,?)''', (f, tile_RValue, tile_GValue, tile_BValue))
+    counter += 1
+    print 'Added', f, '(', counter, '/', len(os.listdir(os.getcwd())), ')'
+
 db.commit()
 db.close()
-
-#resize images
-#put rgb/hash data into table
